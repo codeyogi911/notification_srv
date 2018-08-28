dd3064a9c077722106430c0461d1311ad32d55a8
Shashwat Jain <shashwatjain@icloud.com>
2018-08-29 02:42:01 +0800

    docker setup

---
+++ src/main/java/com/notif/srv/NotificationSrvApplication.java
@@ -0,0 +1,12 @@
+package com.notif.srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationSrvApplication.class, args);
+	}
+}
